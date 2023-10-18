920835e5181919ed61a116b53ef284fe716c4f37
João Paulo N. Rodrigues <joaopaulonr.ti@gmail.com>
2023-10-18 17:00:10 -0300

    added auto scaling group

--- Terraform/autoScaling.tf
+++ Terraform/autoScaling.tf
@@ -1,8 +1,12 @@
 # Model da Instância
 resource "aws_launch_template" "pratice_template" {
-  image_id      = var.ami
-  instance_type = var.instance_type
-  user_data     = filebase64("../Scripts/docker_install.sh")
+  name_prefix            = "pratice_template"
+  description            = "modelo de instância"
+  image_id               = var.ami
+  instance_type          = var.instance_type
+  user_data              = filebase64("../Scripts/docker_install.sh")
+  vpc_security_group_ids = [aws_security_group.pratice_security_group_private.id]
+  key_name               = var.key_name
 
   block_device_mappings {
     device_name = "/dev/sdf"
@@ -11,10 +15,6 @@ resource "aws_launch_template" "pratice_template" {
       volume_type = var.volume_type
       volume_size = var.volume_size
     }
-  }
-
-  network_interfaces {
-    associate_public_ip_address = false
   }
 
   tag_specifications {
@@ -34,4 +34,17 @@ resource "aws_launch_template" "pratice_template" {
       CostCenter = "C092000004"
     }
   }
+  depends_on = [aws_security_group.pratice_security_group_private]
+}
+
+# Auto scaling Group
+resource "aws_autoscaling_group" "pratice_asg" {
+  min_size = 2
+  max_size = 8
+  desired_capacity = 2
+  launch_template {
+    id = aws_launch_template.pratice_template.id
+  }
+  vpc_zone_identifier = [ aws_subnet.pratice_subnet_private01.id,aws_subnet.pratice_subnet_private02.id ]
+  health_check_grace_period = 300
 }
