4289c5bb17c0616725ccb3b0621e701f911c01ee
João Paulo N. Rodrigues <joaopaulonr.ti@gmail.com>
2023-10-18 16:59:48 -0300

    updated script

--- Scripts/docker_install.sh
+++ Scripts/docker_install.sh
@@ -6,14 +6,18 @@ mkdir /srv/solution
 timedatectl set-timezone America/Fortaleza
 
 # Instalação do Docker e do Compose
-yum install docker
-systemctl enable docker.service
-systemctl start docker.service
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
-mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-chmod -v +x /usr/local/bin/docker-compose
+amazon-linux-extras install nginx1 -y
+systemctl enable nginx.service
+systemctl start nginx.service
 
-# Escrita do Docker Compose
-cat <<EOF > /srv/solution/docker-compose.yml
-#script aqui!!!
-EOF
+# yum install docker
+# systemctl enable docker.service
+# systemctl start docker.service
+# wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+# mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+# chmod -v +x /usr/local/bin/docker-compose
+
+# # Escrita do Docker Compose
+# cat <<EOF > /srv/solution/docker-compose.yml
+# #script aqui!!!
+# EOF
