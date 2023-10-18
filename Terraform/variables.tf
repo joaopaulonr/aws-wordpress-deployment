d38e8f153c6fa2ca2eafb50f57c9ee444f30f0be
João Paulo N. Rodrigues <joaopaulonr@gmail.com>
2023-10-18 10:40:06 -0300

    Update variables

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,3 +2,23 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "ami" {
+  type    = string
+  default = "ami-03eb6185d756497f8"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.small"
+}
+
+variable "volume_type" {
+  type    = string
+  default = "gp3"
+}
+
+variable "volume_size" {
+  type    = number
+  default = 8
+}
