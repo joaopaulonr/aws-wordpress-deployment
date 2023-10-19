2ce5e5ad11cb0d9c6236ec1a78765cd498f30be9
João Paulo N. Rodrigues <joaopaulonr.ti@gmail.com>
2023-10-19 14:07:02 -0300

    update vpc

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "pratice_subnet_public01" {
     Name = "pratice_public_subnet01"
   }
 }
+
 resource "aws_subnet" "pratice_subnet_public02" {
   vpc_id            = aws_vpc.pratice_vpc.id
   cidr_block        = "172.21.1.0/24"
@@ -23,20 +24,40 @@ resource "aws_subnet" "pratice_subnet_public02" {
     Name = "pratice_public_subnet02"
   }
 }
-resource "aws_subnet" "pratice_subnet_private01" {
+
+resource "aws_subnet" "pratice_subnet_public03" {
   vpc_id            = aws_vpc.pratice_vpc.id
   cidr_block        = "172.21.2.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "pratice_public_subnet03"
+  }
+}
+
+resource "aws_subnet" "pratice_subnet_private01" {
+  vpc_id            = aws_vpc.pratice_vpc.id
+  cidr_block        = "172.21.3.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "pratice_private_subnet01"
   }
 }
+
 resource "aws_subnet" "pratice_subnet_private02" {
   vpc_id            = aws_vpc.pratice_vpc.id
-  cidr_block        = "172.21.3.0/24"
+  cidr_block        = "172.21.4.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "pratice_private_subnet02"
+  }
+}
+
+resource "aws_subnet" "pratice_subnet_private03" {
+  vpc_id            = aws_vpc.pratice_vpc.id
+  cidr_block        = "172.21.5.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "pratice_private_subnet03"
   }
 }
 
@@ -50,7 +71,7 @@ resource "aws_internet_gateway" "pratice_igw" {
 
 # Elastic IP
 resource "aws_eip" "nat_gateway" {
-  #o ip 
+  #IP elástico para o nat gateway
 }
 
 # NAT Gateway
@@ -96,6 +117,11 @@ resource "aws_route_table_association" "pratice_public_association02" {
   route_table_id = aws_route_table.pratice_public_rt.id
 }
 
+resource "aws_route_table_association" "pratice_public_association03" {
+  subnet_id      = aws_subnet.pratice_subnet_public03.id
+  route_table_id = aws_route_table.pratice_public_rt.id
+}
+
 resource "aws_route_table_association" "pratice_private_association01" {
   subnet_id      = aws_subnet.pratice_subnet_private01.id
   route_table_id = aws_route_table.pratice_private_rt.id
@@ -103,5 +129,10 @@ resource "aws_route_table_association" "pratice_private_association01" {
 
 resource "aws_route_table_association" "pratice_private_association02" {
   subnet_id      = aws_subnet.pratice_subnet_private02.id
+  route_table_id = aws_route_table.pratice_private_rt.id
+}
+
+resource "aws_route_table_association" "pratice_private_association03" {
+  subnet_id      = aws_subnet.pratice_subnet_private03.id
   route_table_id = aws_route_table.pratice_private_rt.id
 }
