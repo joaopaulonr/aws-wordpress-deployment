729dc5b456faca1327b1d1e9439ee85dc1733091
João Paulo N. Rodrigues <joaopaulonr.ti@gmail.com>
2023-10-20 16:59:39 -0300

    update outputs

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,3 +1,4 @@
+#
 output "rds_endpoint" {
   value = aws_db_instance.pratice_db.endpoint
 }
@@ -7,9 +8,14 @@ output "rds_user" {
 }
 
 output "rds_passwd" {
-  value = aws_db_instance.pratice_db.password
+  value     = aws_db_instance.pratice_db.password
+  sensitive = true
 }
 
 output "rds_name" {
   value = aws_db_instance.pratice_db.db_name
+}
+
+output "dns_name" {
+  value = aws_efs_file_system.pratice_efs.dns_name
 }
