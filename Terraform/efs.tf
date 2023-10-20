d6644ceed7650d22c8deb0b32592c42c0c1a3d8f
João Paulo N. Rodrigues <joaopaulonr.ti@gmail.com>
2023-10-20 16:58:28 -0300

    update efs

---
+++ Terraform/efs.tf
@@ -0,0 +1,25 @@
+#
+resource "aws_efs_file_system" "pratice_efs" {
+  creation_token   = "pratice-efs"
+  performance_mode = "generalPurpose"
+  encrypted        = true
+}
+
+#
+resource "aws_efs_mount_target" "pratice_efs_mount_tg01" {
+  file_system_id  = aws_efs_file_system.pratice_efs.id
+  security_groups = [aws_security_group.pratice_security_group_efs.id]
+  subnet_id       = aws_subnet.pratice_subnet_private01.id
+}
+
+resource "aws_efs_mount_target" "pratice_efs_mount_tg02" {
+  file_system_id  = aws_efs_file_system.pratice_efs.id
+  security_groups = [aws_security_group.pratice_security_group_efs.id]
+  subnet_id       = aws_subnet.pratice_subnet_private02.id
+}
+
+resource "aws_efs_mount_target" "pratice_efs_mount_tg03" {
+  file_system_id  = aws_efs_file_system.pratice_efs.id
+  security_groups = [aws_security_group.pratice_security_group_efs.id]
+  subnet_id       = aws_subnet.pratice_subnet_private03.id
+}
